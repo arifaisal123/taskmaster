f735ae2d50bcd510adcc6ba3caa851ddba9e2c2a
arifaisal123 <arifaisal123@gmail.com>
2023-01-19 12:10:41 +0600

    First commit

---
+++ app.py
@@ -0,0 +1,68 @@
+# Tutorial Credit: https://www.youtube.com/watch?v=Z1RJmh_OqeA&list=WL&index=1&t=8s (FreeCodeCamp)
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# Initializes Flask with the file name
+app = Flask(__name__)
+
+# Initializes the location of the database; test.db is the name of the database file we are creating
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db" # Three forward slashes mean relative location; if 4 then meant absolute path
+
+# Initializes database
+with app.app_context():
+    db = SQLAlchemy(app) 
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
+
+# Routes towards the / url
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        task_content = request.form["content"]
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue adding your task"
+
+    else:
+        # Displays all the tasks in the database table
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting that task!"
+
+@app.route("/update/<int:id>", methods=["POST", "GET"])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue updating your task!"
+    else:
+        return render_template("update.html", task = task)
+
+# Runs the app file, debug True will generate any error message
+if __name__ == "__main__":
+    app.run(debug=True)
